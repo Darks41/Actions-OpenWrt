fcc2ac7237be6ed09c688c14175a82414873f686
Darks41 <118961941+Darks41@users.noreply.github.com>
2026-06-01 19:33:40 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-# =============================================
-# diy-part2.sh - OpenWRT build customization
-# 修复 netdata v1.38.1 C++ 标准冲突
-# =============================================
-# 问题：
-#   netdata aclk/schema-wrappers/Makefile.in 硬编码 -std=c++11
-#   新版 protobuf/abseil-cpp 要求 C++14+，导致编译失败
-#   #error "C++ versions less than C++14 are not supported."
 #
-# 修复：
-#   在 Build/Configure 完成后，sed 替换生成的 Makefile 中的 -std=c++11
-# =============================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 移除旧版本 netdata
+#rm -rf feeds/packages/admin/netdata
+# 临时克隆官方最新 packages 仓库，并提取最新版 netdata
+#git clone --depth=1 https://github.com/openwrt/packages.git temp_packages
+#cp -r temp_packages/admin/netdata feeds/packages/admin/netdata
+#rm -rf temp_packages
 
 NETDATA_MAKEFILE="feeds/packages/admin/netdata/Makefile"
 [ -f "package/feeds/packages/netdata/Makefile" ] && NETDATA_MAKEFILE="package/feeds/packages/netdata/Makefile"
